d7f20ef8f8fbcf8cb098ede1fdb0d9d5d4711768
e <13696426353@163.com>
2020-11-26 21:57:04 +0800

    nodejs_02

--- nodjs/demo/requestHandlers.js
+++ nodjs/demo/requestHandlers.js
@@ -1,4 +1,5 @@
-var exec = require("child_process").exec;
+// var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response){
     console.log("Request handler 'start' was called.");
@@ -13,20 +14,38 @@ function start(response){
     // return "Hello Start";
 
     // var content = "empty";
-    exec("ls-lah",
-        function(error, stdout, stderr){
-        response.writeHead(200, {"Content-Type":"text/plain"});
-        response.write(stdout);
-        response.end();
-    });
+
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html;'+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
+
+    // exec("ls-lah",
+    //     function(error, stdout, stderr){
+    //     response.writeHead(200, {"Content-Type":"text/html"});
+    //     response.write(stdout);
+    //     response.end();
+    // });
+
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.write(body);
+    response.end();
 
     // return content;
 };
 
-function upload(response){
+function upload(response, postData){
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent: " + querystring.parse(postData).text);
     response.end();
     // return "Hello upload";
 };
